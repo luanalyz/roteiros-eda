2e604984c67a8a4026989deb1c902887db5005d6
luanalyz <140714040+luanalyz@users.noreply.github.com>
2025-03-18 21:11:44 -0300

    fixing radix

--- src/sorting/linear/src/main/java/sorting/RadixSort.java
+++ src/sorting/linear/src/main/java/sorting/RadixSort.java
@@ -8,27 +8,27 @@ public class RadixSort {
 	// Caso precise do counting sort, use o que você já implementou na outra classe.
 	public int[] radixSort(int[] v) {
 		if (v == null || v.length == 0) {
-            return new int[0];
-        }
+			return new int[0];
+        	}
 
 		int[] output = Arrays.copyOf(v, v.length);
-        int max = getMax(v);
+        	int max = getMax(v);
 
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            CountingSort.classicCountingSort(output, exp);
-        }
-
+	        for (int exp = 1; max / exp > 0; exp *= 10) {
+	            CountingSort.classicCountingSort(output, exp);
+	        }
+	
 		return output;
 	}
 
 	private static int getMax(int[] array) {
-        int max = array[0];
-        for (int num : array) {
-            if (num > max) {
-                max = num;
-            }
-        }
-        return max;
-    }
+	        int max = array[0];
+	        for (int num : array) {
+	            if (num > max) {
+	                max = num;
+	            }
+	        }
+	        return max;
+    	}
 
 }
